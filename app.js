8caf939d477a46ea10db834ce733f70f96497f60
crehds <carlos15erh@gmail.com>
2026-05-22 21:04:25 -0500

    Agregar estructura básica de la aplicación con EJS, incluyendo encabezado, pie de página y vista principal

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 import express from "express";
-
+import expressEjsLayout from "express-ejs-layouts";
 const app = express();
 const PORT = 3000;
 
 app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.use(expressEjsLayout);
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor encendido y escuchando en el puerto ${PORT}`);
