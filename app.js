fb932bd2f431a13439cc408921c24a965d4de9da
crehds <carlos15erh@gmail.com>
2026-05-25 18:51:17 -0500

    feat: add new routes and views for product categories, cart, checkout, and order confirmation
    - Added routes for product, category, about, cart, privacy, terms, checkout, and order confirmation pages in app.js.
    - Created 404 error page view.
    - Developed views for about, cart, category, checkout, order confirmation, privacy, product, and terms.
    - Updated index.ejs and footer.ejs to link to new routes.
    - Implemented a .gitignore entry for fullstock-static-files.

--- app.js
+++ app.js
@@ -12,6 +12,45 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/product", (req, res) => {
+  res.render("product");
+});
+
+app.get("/category", (req, res) => {
+  res.render("category");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/cart", (req, res) => {
+  res.render("cart");
+});
+
+app.get("/privacy", (req, res) => {
+  res.render("privacy");
+});
+
+app.get("/terms", (req, res) => {
+  res.render("terms");
+});
+
+app.get("/checkout", (req, res) => {
+  res.render("checkout");
+});
+
+app.get("/order-confirmation", (req, res) => {
+  res.render("order-confirmation");
+});
+
+app.use((error, req, res, _) => {
+  console.error(error);
+  console.log("hola");
+
+  res.render("404");
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor encendido y escuchando en el puerto ${PORT}`);
 });
