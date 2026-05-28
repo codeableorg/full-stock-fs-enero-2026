a09a9feff729410daeb2b0ce74cdf443a4063612
crehds <carlos15erh@gmail.com>
2026-05-27 19:13:57 -0500

    feat: agregar nuevos productos y mejorar el filtrado por precio en el controlador de categorías

--- handler/categoryHandler.js
+++ handler/categoryHandler.js
@@ -5,6 +5,11 @@ const DATA_PATH = path.join("data", "data.json");
 
 export async function categoryHandler(req, res) {
   const { slug } = req.params;
+  const { minPrice: minPriceQuery, maxPrice: maxPriceQuery } = req.query;
+
+  const minPrice = minPriceQuery ? Number(minPriceQuery) * 100 : -Infinity;
+  const maxPrice = maxPriceQuery ? Number(maxPriceQuery) * 100 : Infinity;
+
   const dataJson = await fs.readFile(DATA_PATH, "utf-8");
 
   const data = JSON.parse(dataJson);
@@ -14,9 +19,13 @@ export async function categoryHandler(req, res) {
     return res.status(404).render("404");
   }
 
-  const products = data.products.filter(
-    (product) => product.categoryId === category.id,
-  );
+  const products = data.products.filter((product) => {
+    const belongsToCategory = product.categoryId === category.id;
+    const meetsMinPrice = product.price >= minPrice;
+    const meetsMaxPrice = product.price <= maxPrice;
+   
+    return belongsToCategory && meetsMinPrice && meetsMaxPrice;
+  });
 
   res.render("category", { category, products });
 }
