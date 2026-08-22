bf6588b1c1f04bd7a89da8d2c87c8ece1d2de10f
crehds <carlos15erh@gmail.com>
2026-08-21 20:59:55 -0500

    first step TS migration

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,31 @@
 import js from "@eslint/js";
 import globals from "globals";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
 
-export default [
-  js.configs.recommended,
-  {
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      globals: {
-        ...globals.node,
-        ...globals.browser,
+export default defineConfig({
+  files: ["**/*.{js,ts}"],
+  extends: [
+    js.configs.recommended,
+    tseslint.configs.recommended,
+    {
+      languageOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        globals: {
+          ...globals.node,
+          ...globals.browser,
+        },
+      },
+      rules: {
+        "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+        "@typescript-eslint/no-unused-vars": [
+          "warn",
+          { argsIgnorePattern: "^_" },
+        ],
+        "prefer-const": "error",
+        eqeqeq: ["error", "always"],
       },
     },
-    rules: {
-      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-      "prefer-const": "error",
-      eqeqeq: ["error", "always"],
-    },
-  },
-];
+  ],
+});
