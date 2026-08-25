266bb136a5fbd41a9108ecec4086fe256ab93d9e
crehds <carlos15erh@gmail.com>
2026-08-24 19:51:59 -0500

    feat(types): añadir tipos de modelos de dominio basados en el data.json

---
+++ types/index.ts
@@ -0,0 +1,66 @@
+export interface Category {
+  id: number;
+  name: string;
+  slug: string;
+  imgSrc: string;
+  alt: string;
+  description: string;
+}
+
+export interface Product {
+  id: number;
+  name: string;
+  imgSrc: string;
+  price: number;
+  description: string;
+  categoryId: number;
+  features: string[];
+}
+
+export interface Cart {
+  id: number;
+  userId?: number;
+  items: CartItem[];
+}
+
+export interface CartItem {
+  productId: number;
+  quantity: number;
+}
+
+export interface User {
+  id: number;
+  email: string;
+  password: string;
+}
+
+export interface Order {
+  id: number;
+  userId?: number;
+  items: OrderItem[];
+  shippingInfo: ShippingInfo;
+  total: number;
+  status: string;
+  createdAt: string;
+}
+
+export interface OrderItem {
+  productId: number;
+  name: string;
+  price: number;
+  imgSrc: string;
+  quantity: number;
+}
+
+export interface ShippingInfo {
+  email: string;
+  firstName: string;
+  lastName: string;
+  company: string;
+  address: string;
+  city: string;
+  country: string;
+  region: string;
+  zipCode: string;
+  phone: string;
+}
