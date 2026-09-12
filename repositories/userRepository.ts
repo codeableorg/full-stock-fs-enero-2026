6e5ca344e31deb8b79549e4591d2435585e9dc19
crehds <carlos15erh@gmail.com>
2026-09-11 21:04:49 -0500

    refactor(users): guardar usuarios en PostgreSQL

--- repositories/userRepository.ts
+++ repositories/userRepository.ts
@@ -1,30 +1,29 @@
-import { getDb, getNextId, saveDb } from "../db.ts";
-import type { Db, User } from "../types/index.ts";
+import { pool } from "../db/pool.ts";
+import type { User } from "../types/index.ts";
 
-function getUsers(db: Db) {
-  if (!Array.isArray(db.users)) {
-    db.users = [];
-  }
-
-  return db.users;
-}
+const SELECT_USER = `SELECT * from users`;
 
 export async function findByEmail(email: string) {
-  const db = await getDb();
-  return getUsers(db).find((user) => user.email === email) || null;
+  const { rows } = await pool.query<User>(`${SELECT_USER} WHERE email = $1`, [
+    email,
+  ]);
+  return rows.at(0);
 }
 
 export async function findById(userId: number) {
-  const db = await getDb();
-  return getUsers(db).find((user) => user.id === userId) || null;
+  const { rows } = await pool.query<User>(`${SELECT_USER} WHERE id = $1`, [
+    userId,
+  ]);
+  return rows.at(0);
 }
 
 export async function create(userData: Omit<User, "id">) {
-  const db = await getDb();
-  const nextId = await getNextId("users");
-  // Todo:  restringir TS pues con un objeto y el spread esto pasa
-  const newUser: User = { id: nextId, ...userData };
-  getUsers(db).push(newUser);
-  await saveDb(db);
-  return newUser;
+  const { rows } = await pool.query<User>(
+    `INSERT INTO users(email, password)
+    VALUES ($1,$2)
+    RETURNING *`,
+    [userData.email, userData.password],
+  );
+
+  return rows.at(0);
 }
